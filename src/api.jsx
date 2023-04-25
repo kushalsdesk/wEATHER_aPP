d699bd20d5aa1582ad83a5e1ea7e7584ccacb48a
Kushal Mondal <kushal99mondal@gmail>
2023-04-25 10:33:17 +0530

    recievingData

---
+++ src/api.jsx
@@ -0,0 +1,12 @@
+export const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'content-type': 'application/octet-stream',
+		'X-RapidAPI-Key': '1c92f1a8b1msh2c2895873151217p18a6dbjsna19dab27e7bb',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+
---
+++ src/components/search/Search.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { url, geoApiOptions } from "../../api";
+
+const Search = ({ onSearchChange }) => {
+
+  const [search, setSearch] = useState(null);
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData)
+  };
+
+  const loadOptions = async (inputValue) => {
+    try {
+      const response = await fetch(`${url}?minPopulation=100000&namePrefix=${inputValue}`, geoApiOptions);
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+
+    /* /* return fetch(
+      `${url}?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch( (err) => console.log(err));*/
+
+
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city..."
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  )
+}
+
+export default Search;
