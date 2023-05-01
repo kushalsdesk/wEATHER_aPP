7488a9dcf797701099d9660e7df812d1ffab0fc7
Kushal Mondal <kushal99mondal@gmail>
2023-05-01 15:40:34 +0530

    imageUpdated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className='header'>       
-      <img src="/public/icons/weather.png" alt="" />
+      <img src={`icons/weather.png`} alt="" />
 
       <p>Get Your Weather Update</p>
     </div>
